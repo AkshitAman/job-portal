49ec38c21136e5e49d91a3f41c263a494e1a0479
harsh <nh1750501@gmail.com>
2025-11-11 23:09:19 +0530

    Fixed bugs and added routing for login and register

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
-import Job from "../models/jobModel";
-import User from "../models/userModel";
+import Job from "../models/jobModel.js";
+import User from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 
