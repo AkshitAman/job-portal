435f6ec732f77e956caabbcc56b33ea282f56a6c
harsh <nh1750501@gmail.com>
2025-11-21 13:51:50 +0530

    Fixed routing in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import { Toaster } from "react-hot-toast";
 import Register from "./pages/Register";
+import RecruiterRegister from "./pages/RecruiterRegister";
+import RecruiterDashboard from "./pages/RecruiterDashboard";
 
 function App() {
   return (
@@ -9,9 +12,12 @@ function App() {
       <Toaster position="top-right" />
 
       <Routes>
+         <Route path="/" element={<Home />} />
         {/* Auth Routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+              <Route path="/register-company" element={<RecruiterRegister />} />
+        <Route path="/recruiter-dashboard" element={<RecruiterDashboard />} />
       </Routes>
     </div>
   );
